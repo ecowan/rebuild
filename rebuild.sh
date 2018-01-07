c3b779edb68187ae01e14ec56b5ec3a5a590d160
Ethan <ethan24@gmail.com>
2018-01-07 00:32:48 -0500

    First commit

---
+++ rebuild.sh
@@ -0,0 +1,18 @@
+# So you've screwed up the computer again....
+
+sudo apt install -y zsh git python3-pip emacs htop openssh-server
+sudo pip3 install virtualenv
+
+cd ~/Downloads &&
+    wget "https://download.jetbrains.com/idea/ideaIU-2017.3.2.tar.gz" &&
+    tar xfz ideaIU-2017.3.2.tar.gz
+    gvfs-trash ideaIU-2017.3.2.tar.gz &&
+    mkdir ~/Applications &&
+    mv ideaIU-2017.3.2/ ~/Applications &&
+    cd ~
+
+
+# oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
